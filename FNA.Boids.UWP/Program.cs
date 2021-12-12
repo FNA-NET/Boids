e80974c3665bb084f74aca7bba70d227e205dc1e
ryancheung <ryancheung.go@gmail.com>
2021-12-12 17:44:33 +0800

    Added UWP project

---
+++ FNA.Boids.UWP/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using SDL2;
+
+namespace FNA.Boids.UWP
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            realArgs = args;
+            SDL.SDL_SetHint("SDL_WINRT_HANDLE_BACK_BUTTON", "1");
+            SDL.SDL_main_func mainFunction = FakeMain;
+            SDL.SDL_WinRTRunApp(mainFunction, IntPtr.Zero);
+        }
+
+        static string[] realArgs;
+        static int FakeMain(int argc, IntPtr argv)
+        {
+            RealMain(realArgs);
+            return 0;
+        }
+
+        static void RealMain(string[] args)
+        {
+            using (var g = new Game1())
+            {
+                g.Run();
+            }
+        }
+    }
+}
